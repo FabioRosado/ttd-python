a8f769238b6ff44c4f979ea21c8ddf99362eec20
FabioRosado <fabioglrosado@gmail.com>
2017-11-03 09:24:12 +0000

    Refactor using URL includes

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import url
-from django.contrib import admin
+"""URLS that apply to the entire site"""
+
+from django.conf.urls import include, url
+# from django.contrib import admin
 from lists import views
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     url(r'^$', views.home_page, name='home'),
-    url(r'^lists/(\d+)/$', views.view_list, name='view_list'),
-    url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
-    url(r'^lists/new$', views.new_list, name='new_list'),
+    url(r'^lists/', include('lists.urls')),
 ]
